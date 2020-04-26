b66f13c701ae43cf3399dddd222a22c852a72e31
anoorzi <anoorzi123@gmail.com>
2020-04-26 03:47:21 -0400

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,49 @@
-// Code EyesOnMe Component Here
+// My version!!!! better!
+// import React from 'react';
+
+// class EyesOnMe extends React.Component{
+//    state = {
+//        event: 'blur'
+//    }
+//     handleClick = () => {
+//         if(this.state.event==='blur'){
+//             console.log('Hey! Eyes on me!')
+//             this.setState({
+//                 event: 'focus'
+//             })
+//         }
+//         if(this.state.event==='focus'){
+//             console.log('Good!')
+//             this.setState({
+//                 event: 'blur'
+//             })
+
+//         }
+//    }
+//     render(){
+//        return (
+//            <button onClick={this.handleClick}>{this.state.event}</button>
+//        ) 
+//     }
+// }
+
+// export default EyesOnMe
+  
+import React from 'react';
+
+class EyesOnMe extends React.Component {
+
+  onFocus = () => console.log('Good!')
+
+  onBlur = () => console.log('Hey! Eyes on me!')
+
+  render() {
+    return (
+      <button onFocus={this.onFocus} onBlur={this.onBlur}>
+        Eyes on me, please!
+      </button>
+    )
+  }
+}
+
+export default EyesOnMe;
